668031acb4175562ab218699285a009fdf354e3d
Stevie Ped <stephen.pederson.au@gmail.com>
2024-01-23 20:34:15 +1030

    Set overlapsByVar as S4 method

--- tests/testthat/test_overlapsByVar.R
+++ tests/testthat/test_overlapsByVar.R
@@ -2,11 +2,11 @@ test_that("overlapsByVar behaves correctly", {
 
     grl <- splitAsList(gtf, gtf$type)
     ## Expected outputs
-    df <- overlapsByVar(grl, var)
-    expect_true(is(df, "data.frame"))
-    expect_equal(rownames(df), names(grl))
-    expect_equal(colnames(df), c("Deletion", "Insertion", "SNV", "Total"))
-    expect_true(all(df$Total == c(98, 98, 38)))
+    mat <- overlapsByVar(grl, var)
+    expect_true(is(mat, "matrix"))
+    expect_equal(rownames(mat), names(grl))
+    expect_equal(colnames(mat), c("Deletion", "Insertion", "SNV", "Total"))
+    expect_true(all(mat[,"Total"] == c(98, 98, 38)))
 
     ## And the errors
     expect_error(overlapsByVar(GRanges()))
