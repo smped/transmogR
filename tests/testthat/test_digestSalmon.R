2a4e3127861e4581ba471dcc46e1ce85ed89400a
Stevie Ped <stephen.pederson.au@gmail.com>
2025-02-05 18:39:58 +1030

    Testing speed-up

--- tests/testthat/test_digestSalmon.R
+++ tests/testthat/test_digestSalmon.R
@@ -2,11 +2,12 @@ f <- system.file("extdata/salmon_test", package = "transmogR")
 
 test_that("assayFromQuants returns correct values",{
 
+    ids <- c("t1", "t2")
     quants <- list(
         A = data.frame(Name = c("t1", "t2"), od = rnorm(2)),
         B = data.frame(Name = "t1", od = rnorm(1))
     )
-    mat <- .assayFromQuants(quants, "od", 0)
+    mat <- .assayFromQuants(quants, "od", ids, 0)
     expect_true(is(mat, "matrix"))
     expect_true(is.double(mat))
     expect_equal(rownames(mat), c("t1", "t2"))
@@ -16,10 +17,19 @@ test_that("assayFromQuants returns correct values",{
 })
 
 test_that("salmon digestion is smooth", {
- se <- suppressMessages(digestSalmon(f))
+ se <- suppressMessages(digestSalmon(f, length_as_assay = TRUE))
  expect_true(is(se, "SummarizedExperiment"))
  expect_equal(dim(se), c(2L, 1L))
  expect_equal(metadata(se)$resampleType, "gibbs")
+ expect_equal(
+     c("counts", "scaledCounts", "TPM", "effectiveLength", "length"),
+     SummarizedExperiment::assayNames(se)
+ )
+
+ se <- suppressMessages(digestSalmon(f, extra_assays = NULL))
+ expect_equal(
+     c("counts", "scaledCounts"), SummarizedExperiment::assayNames(se)
+ )
 
 })
 
