36c54ecd5d2f5c68985ce8134be2ed71570e385c
Stevie Ped <stephen.pederson.au@gmail.com>
2025-10-08 00:35:36 +1030

    corrected tests

--- tests/testthat/test_upsetVarByCol.R
+++ tests/testthat/test_upsetVarByCol.R
@@ -1,17 +1,25 @@
 test_that("upsetVarByCol behaves correctly", {
     grl <- splitAsList(gtf, gtf$type)
-    p <- upsetVarByCol(grl$exon, var, intersection_args = list(fill = "blue"))
+    p <- upsetVarByCol(
+        grl$exon, var, fill = "gene_type", fill_scale = scale_fill_brewer(palette = "Set1")
+    )
     expect_true(is(p, "patchwork"))
-    expect_true(is(p[[1]], "spacer"))
-    expect_true(is(p[[2]], "gg"))
-    expect_true(p[[2]]$geom[[1]]$aes_params$fill == "blue")
-    expect_true(is(p[[2]]$geom[[1]]$geom, "GeomBar"))
-    expect_true(p[[2]]$labels$title == "37 Overlap; 75 No Overlap (transcript_id)")
-    expect_true(is(p[[3]]$layers[[3]]$geom, "GeomText"))
-
-    ## Turn off counts
-    p <- upsetVarByCol(grl$exon, var, set_counts = FALSE)
-    expect_true(length(p[[3]]$layers) == 2)
+    expect_true(is_waiver(p[[1]]@data))
+    expect_equal(
+        p@meta$patches$annotation$title, "37 Overlap; 75 No Overlap (transcript_id)"
+    )
+    expect_equal(
+        as_label(p[[2]]@layers$geom_bar$mapping$fill), "gene_type"
+    )
+    expect_equal(
+        as_label(p[[3]]@layers$geom_bar$mapping$fill), "gene_type"
+    )
+    expect_true(is(p[[2]]@scales$scales[[3]], "ScaleDiscrete"))
+    expect_equal(p[[2]]@scales$scales[[3]]$aesthetics, "fill")
+    expect_equal(
+        p[[2]]@scales$scales[[3]]$palette(3), c("#E41A1C", "#377EB8", "#4DAF4A")
+    )
+    expect_true(is(p[[3]]@guides$guides$fill, "GuideNone"))
 
     ## Errors
     expect_error(upsetVarByCol(grl))
